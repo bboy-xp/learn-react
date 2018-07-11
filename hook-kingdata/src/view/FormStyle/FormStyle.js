e9cadf5b107d394ddecdf4a66e314e35bcf814cb
unknown <1169470657@qq.com>
2018-07-11 23:47:53 +0800

    新增表单样式界面

--- hook-kingdata/src/view/FormStyle/FormStyle.js
+++ hook-kingdata/src/view/FormStyle/FormStyle.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import { Radio } from 'zent';
+import { Radio, Checkbox, Select } from 'zent';
 import('./FormStyle.css');
+const CheckboxGroup = Checkbox.Group;
 
 const RadioGroup = Radio.Group;
+const data = [
+  { id: 1, name: 'Option 1' },
+  { id: 2, name: 'Option 2' },
+  { id: 3, name: 'Option 3' }
+];
 export default class FormStyle extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      checkedList: [],
+    }
+    this.onChange = this.onChange.bind(this);
+    this.showOption = this.showOption.bind(this);
   }
 
+  onChange(checkedList) {
+    this.setState({ checkedList });
+  }
+  showOption() {
+    console.log(111);
+  }
+
+
   render() {
+    const { checkedList } = this.state;
     return (
       <div>
-        <div>
-          <RadioGroup value="male">
-            <Radio value="male">男</Radio>
-            <Radio value="female">女</Radio>
-          </RadioGroup>
-        </div>
         <div>
           <div className="inputQuestion">· 单行文本</div>
           <input className="inputBox" />
@@ -34,6 +48,30 @@ export default class FormStyle extends Component {
         <div>
           <div className="inputQuestion">· 邮箱</div>
           <input className="inputBox" />
+        </div>
+        <div>
+          <div className="inputQuestion">单项选择</div>
+          <RadioGroup>
+            <Radio value="male">男</Radio>
+            <Radio value="female">女</Radio>
+          </RadioGroup>
+        </div>
+        <div>
+          <div className="inputQuestion">多项选择</div>
+          <CheckboxGroup value={checkedList} onChange={this.onChange}>
+            <Checkbox value="Apple">苹果</Checkbox>
+            <Checkbox value="Pear">梨子</Checkbox>
+            <Checkbox value="Orange">橘子</Checkbox>
+          </CheckboxGroup>
+        </div>
+        <div>
+          <div className="inputQuestion">下拉框</div>
+          <Select
+            data={data}
+            optionValue="id"
+            optionText="name"
+            onChange={this.showOption}
+          />,
         </div>
 
       </div>
